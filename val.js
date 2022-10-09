4fca830f0a7e69849439c3b85c19df600e8495b7
Divakar <81960004+diva-21@users.noreply.github.com>
2022-10-09 05:36:33 +0530

    update doc

--- val.js
+++ val.js
@@ -1,6 +1,7 @@
-const form=document.getElementById('form')
-const username=document.getElementById('username')
-const email=document.getElementById('email')
-const inst=document.getElementById('clg')
-
+// const form=document.getElementById('form')
+// const username=document.getElementById('username')
+// const email=document.getElementById('email')
+// const inst=document.getElementById('clg')
+// should complete this
+// and connect it to front part on index html
 
